3751b94e0d62804d07de30eb3e9c5e70a8beed76
A Z M Monir Hossain <monir@AZMs-MacBook-Pro.local>
2023-04-17 17:20:35 +0600

    __notes_

---
+++ __steps_.js
@@ -0,0 +1,35 @@
+/**
+ * 01. create firebase  project
+ * 02. create web app
+ * 03. npm i firebase
+ * 04. save firebase config and  export
+ * 05. Build > Authentication > get started > enable sign in method
+ * 06. create sign up and login routes
+ *
+ */
+/**
+ * 01. Create a context provider
+ * 02. Create a context  and set provider
+ * 03. set the children props
+ * 04.
+ * * */
+/**
+ * HOSTING
+ * One time per pc
+ * 1. npm install -g firebase-tools
+ * 2. firebase login
+ *
+ * for each project one time
+ * 1. firebase init
+ * 2. proceed
+ * 3. hosting: firebase (up and down arrow) use space bar for select
+ * 4. existing project
+ * 5. select the project carefully
+ * 6. which project as public directory: dist
+ * 7. single page application: yes
+ * 8. continuous deployment: no
+ * 
+ * for every time deploy
+ * 1. npm run build
+ * 2. firebase deploy
+ * * */
